89964637ab9bdfff4fad8c187e0e1263893dae36
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-07-04 21:07:04 +0200

    Feat : Added Domain name as well as their IP Address

--- DNS_Lookup_Tool.py
+++ DNS_Lookup_Tool.py
@@ -9,3 +9,19 @@ print("=== DNS Lookup Tool ===")
 print("=======================")
 
 domain = input("\nEnter a domain (e.g. google.com): ")
+
+# _________________________________________________________________________________________________
+
+try:
+
+    ip_address = socket.gethostbyname(domain)
+
+    print("\n=== DNS Information ===\n")
+
+    print(f"Domain Name : {domain}")
+    print(f"IP Address  : {ip_address}")
+    
+except socket.gaierror:
+    print("\nError: Unable to detect the domain")
+    
+# _________________________________________________________________________________________________
