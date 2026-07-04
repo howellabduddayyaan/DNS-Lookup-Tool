3d17b0f4e5468cf8c4f4b1ceaccb905b925a6f13
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-07-04 19:58:53 +0200

    Feat : Created files

---
+++ DNS_Lookup_Tool.py
@@ -0,0 +1,11 @@
+# =======================
+# === DNS Lookup Tool ===
+# =======================
+
+import socket
+
+print("\n=======================")
+print("=== DNS Lookup Tool ===")
+print("=======================")
+
+domain = input("\nEnter a domain (e.g. google.com): ")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DNS-Lookup-Tool
+
 In this repository I will build a DNS Lookup Tool using python.
